e5c5eaf03eeaf33c7381bc3dab3e82a438dddaf9
gonnem2 <gonne.minecraft@mail.ru>
2024-07-05 17:04:50 +0300

    Удален ненужный парсер резюме

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sqlite3
-from fastapi import FastAPI, Query, Request, Depends
+from fastapi import FastAPI, Query, Request
 import sqlite3 as sq
-from rezume import rezume
 from fastapi.templating import Jinja2Templates
 from VAKANS import vakancy
 
@@ -52,33 +51,6 @@ def get_vakans(request: Request,
 
 connn = sq.connect('rezume.db', check_same_thread=False)
 cursor_ = connn.cursor()
-
-
-@app.post('/get_rezume')
-def get_rezume(search: str = Query(), age_from: int = Query(), age_to: int = Query(), salary_from: int = Query(),
-               salary_to: int = Query(), experience: str = Query()):
-    rezume(search, age_from, age_to, salary_from, salary_to, experience)
-    cursor_.execute('SELECT * FROM rezume')
-    vakans = cursor_.fetchall()
-    data = {}
-    c = 0
-    for i in vakans:
-        res = dict()
-        res['id'] = i[0]
-        res['title'] = i[1]
-        res['salary'] = i[2]
-        res['spec'] = i[3]
-        res['grafick'] = i[4]
-        res['naviki'] = i[5]
-        res['education_name'] = i[6]
-        res['zanyatost'] = i[7]
-        res['opit'] = i[8]
-        res['about'] = i[9]
-        res['href'] = i[1]
-        data[str(c)] = res
-        c += 1
-    return data
-
 
 @app.get('/base')
 def get_base_page(request: Request):
